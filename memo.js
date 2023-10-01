579077d6ba7b1d17b79b88f3d94ab5dc8fee44f8
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-10-01 20:56:50 -0300

    se agregan condiciones y solapa de aciertos

--- memo.js
+++ memo.js
@@ -1,5 +1,8 @@
+
 const divMatriz = document.querySelector("#matriz");
-const divMatriz2= document.querySelector("#matriz2");
+const pResultado1=document.querySelector("#resultado1");
+const pResultado2=document.querySelector("#resultado2");
+
 
 const imagenes = [
     "https://cdn.shopify.com/s/files/1/0530/8517/5981/files/ShopByTitle-TJ.png?v=1654799164",
@@ -12,46 +15,92 @@ const imagenes = [
     "https://th.bing.com/th/id/OIP.GXUGr4Lx_UpSqIs6WRt_XQHaD4?pid=ImgDet&rs=1"
 ]
 
+const imagenesContador = {};
+
+let primerElementoSeleccionado = null;
+let aciertos = 0;
+
+
+function inicializarContador() {
+    imagenes.forEach((imagen) => {
+        imagenesContador[imagen] = 0;
+    });
+}
+
 function generar() {
     const MAX_FILA = 4;
     const MAX_COL = 4;
 
-    divMatriz.innerHTML = "";
+    inicializarContador();
+    aciertos = 0 + 1;
+    
 
+    divMatriz.innerHTML = "";
 
     for (let fila = 0; fila < MAX_FILA; fila++) {
         divMatriz.innerHTML += `
             <div class="row">
-                ${agregarColumna(fila)}
+                ${agregarColumna()}
             </div>
         `;
     }
 
-
     function agregarColumna() {
-
         let columnas = "";
 
         for (let col = 0; col < MAX_COL; col++) {
-
+            const imgAleatoria = obtenerImagenAleatoria();
             columnas += `
-                <div onclick="seleccionar(this)" class="col casilla">
-                    
+                <div onclick="seleccionar(this)" data-imagen="${imgAleatoria}" class="col casilla">
                 </div>
             `;
         }
 
         return columnas;
     }
+
+    function obtenerImagenAleatoria() {
+        const imagenesDisponibles = imagenes.filter((imagen) => imagenesContador[imagen] < 2);
+        const imgAleatoria = imagenesDisponibles[Math.floor(Math.random() * imagenesDisponibles.length)];
+
+        imagenesContador[imgAleatoria]++;
+
+        return imgAleatoria;
+    }
 }
 
-function seleccionar(elemento, fila, col) {
+function seleccionar(elemento) {
+    const imgURL = elemento.getAttribute("data-imagen");
+    elemento.innerHTML = `<img class="col casilla" src="${imgURL}" alt=""></img>`;
 
-    const imgAleatoria = Math.floor(Math.random() * imagenes.length);
-    const matriz = imagenes[imgAleatoria];
+    if (primerElementoSeleccionado === null) {
+        primerElementoSeleccionado = elemento;
+    } else {
+        if (primerElementoSeleccionado.getAttribute("data-imagen") === imgURL) {
+            
+            setTimeout(() => {
+                pResultado1.innerHTML= "Aciertos: "+(aciertos ++) ;
+                const TOTAL_ACIERTOS = 8;
+                if (aciertos === TOTAL_ACIERTOS) {
+                    pResultado1.innerHTML="FELICITACIONES GANASTE"
+                    pResultado2.innerHTML=`<button onclick="VolveAJugar()">Volver a Jugar</button>`
+                }
+            }, 500);
+            primerElementoSeleccionado = null;
+        } else {
+            
+            setTimeout(() => {
+                primerElementoSeleccionado.innerHTML = "";
+                elemento.innerHTML = "";
+                primerElementoSeleccionado = null;
+            }, 500);
+        }
+    }
 
-  elemento.innerHTML = `<img class="col casilla" src="${matriz}" alt=" ${fila}-${col}"></img>`
 }
 
+function VolveAJugar() {
+    generar()
+}
 
 

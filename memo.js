e20b7f9875b229b490ff32380c30888b76695932
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-10-01 21:48:19 -0300

    se cambia matriz para probar una mejora visual

--- memo.js
+++ memo.js
@@ -11,8 +11,7 @@ const imagenes = [
     "https://th.bing.com/th/id/OIP.H_O_NixMS99jqpxEt1LgbQHaGP?w=199&h=180&c=7&r=0&o=5&pid=1.7",
     "https://th.bing.com/th/id/OIP.DORBGjJbeD_gc5lF6lawGgHaE7?pid=ImgDet&rs=1",
     "https://th.bing.com/th/id/R.26b32043d1bef08bfc049c3fe0002518?rik=A02hhw311Qm%2f4g&riu=http%3a%2f%2f3.bp.blogspot.com%2f_uGq8SSLyPqI%2fTEiclC0saeI%2fAAAAAAAAAEQ%2folzUNJBzfKA%2fs1600%2fTom-and-Jerry-1-1NPDHNRYGS-1024x768.jpg&ehk=2x7JH0Z5XTU8DJrWeiEc%2ftgCjpltMw4Ate9G2NVl06U%3d&risl=&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/OIP.pml9WIRdnYvGWOTfhePvmgHaNJ?pid=ImgDet&rs=1",
-    "https://th.bing.com/th/id/OIP.GXUGr4Lx_UpSqIs6WRt_XQHaD4?pid=ImgDet&rs=1"
+  
 ]
 
 const imagenesContador = {};
@@ -28,11 +27,11 @@ function inicializarContador() {
 }
 
 function generar() {
-    const MAX_FILA = 4;
+    const MAX_FILA = 3;
     const MAX_COL = 4;
 
     inicializarContador();
-    aciertos = 0 + 1;
+    aciertos = 1;
     
 
     divMatriz.innerHTML = "";
@@ -79,8 +78,8 @@ function seleccionar(elemento) {
         if (primerElementoSeleccionado.getAttribute("data-imagen") === imgURL) {
             
             setTimeout(() => {
-                pResultado1.innerHTML= "Aciertos: "+(aciertos ++) ;
-                const TOTAL_ACIERTOS = 8;
+                 pResultado1.innerHTML= "Aciertos: "+(aciertos ++) ;
+                const TOTAL_ACIERTOS = 7;
                 if (aciertos === TOTAL_ACIERTOS) {
                     pResultado1.innerHTML="FELICITACIONES GANASTE"
                     pResultado2.innerHTML=`<button onclick="VolveAJugar()">Volver a Jugar</button>`
@@ -100,6 +99,8 @@ function seleccionar(elemento) {
 }
 
 function VolveAJugar() {
+    pResultado1.innerHTML="Aciertos:";
+    pResultado2.innerHTML="";
     generar()
 }
 

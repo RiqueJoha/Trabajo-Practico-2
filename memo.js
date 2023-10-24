2126da518a653b7f30ed3c574f270fa41dedfd8f
RiqueJoha <riquelme.johanna07@gmail.com>
2023-10-24 11:35:41 -0300

    deshabilito las imágenes que son correctas

--- memo.js
+++ memo.js
@@ -3,7 +3,7 @@ const divMatriz = document.querySelector("#matriz");
 const pResultado1 = document.querySelector("#resultado1");
 const pResultado2 = document.querySelector("#resultado2");
 
-
+const ACIERTOS = "listado de aciertos"
 
 const imagenes = [
     "https://cdn.shopify.com/s/files/1/0530/8517/5981/files/ShopByTitle-TJ.png?v=1654799164",
@@ -17,8 +17,8 @@ const imagenes = [
 
 const TOTAL_ACIERTOS = 6;
 let ImgSeleccionada = 0;
-let clicks = 0;
-let aciertos=0;
+let aciertos = 0;
+listaAciertos = [];
 
 function ContadorImg() {
     imagenes.forEach((imagen) => {
@@ -29,8 +29,8 @@ function ContadorImg() {
 function generar() {
     const MAX_FILA = 3;
     const MAX_COL = 4;
-    
-    
+
+
 
     ContadorImg();
 
@@ -57,7 +57,7 @@ function generar() {
 
         return columnas;
     }
-/* La función obtenerImagenAleatoria filtra las imágenes que no se han mostrado más de dos veces y elige aleatoriamente una de las imágenes disponibles. Luego, al hacer clic en un elemento del tablero, se muestra la imagen correspondiente y se actualiza el contador. Esto garantiza que nunca se muestren más de dos imágenes idénticas en el tablero. */
+    /* La función obtenerImagenAleatoria filtra las imágenes que no se han mostrado más de dos veces y elige aleatoriamente una de las imágenes disponibles. Luego, al hacer clic en un elemento del tablero, se muestra la imagen correspondiente y se actualiza el contador. Esto garantiza que nunca se muestren más de dos imágenes idénticas en el tablero. */
     function obtenerImagenAleatoria() {
         const imgDisp = imagenes.filter((imagen) => imagenes[imagen] < 2);
         const imgAleatoria = imgDisp[Math.floor(Math.random() * imgDisp.length)];
@@ -75,42 +75,43 @@ function seleccionar(elemento) {
     const imgURL = elemento.getAttribute("data-imagen");
     elemento.innerHTML = `<img class="col casilla" src="${imgURL}" alt=""></img>`;
 
+    if (ImgSeleccionada === 0) {
+        ImgSeleccionada = elemento;
+    } else {
+        if (ImgSeleccionada.getAttribute("data-imagen") === imgURL) {
+            elemento.style.pointerEvents = "none"
+            
+            setTimeout(() => {
+                aciertos++
+                pResultado1.innerHTML = "Aciertos: " + (aciertos);
+                listaAciertos.push(aciertos);
+                localStorage.setItem(ACIERTOS, JSON.stringify(listaAciertos))
 
-    if (clicks == 0) {
-        if (ImgSeleccionada === 0) {
-            ImgSeleccionada = elemento;
+                if (aciertos === TOTAL_ACIERTOS) {
+                     
+                    pResultado1.innerHTML = "FELICITACIONES GANASTE"
+                    pResultado2.innerHTML = `
+                    <button onclick="VolveAJugar()">Volver a Jugar</button>  `
+                    elemento.style.pointerEvents = "none" 
+                }
+            }, 100);
+            ImgSeleccionada = 0;
         } else {
-            if (ImgSeleccionada.getAttribute("data-imagen") === imgURL) {
 
-                setTimeout(() => {
-                    aciertos ++;
-                    pResultado1.innerHTML = "Aciertos: " + (aciertos);
-
-                    if (aciertos === TOTAL_ACIERTOS) {
-                        pResultado1.innerHTML = "FELICITACIONES GANASTE"
-                        pResultado2.innerHTML = `
-                        <button onclick="VolveAJugar()">Volver a Jugar</button>
-                        `
-                    }
-                }, 100);
+            setTimeout(() => {
+                ImgSeleccionada.innerHTML = "";
+                elemento.innerHTML = "";
                 ImgSeleccionada = 0;
-            } else {
-
-                setTimeout(() => {
-                    ImgSeleccionada.innerHTML = "";
-                    elemento.innerHTML = "";
-                    ImgSeleccionada = 0;
-                }, 500);
-            }
+            }, 500);
         }
     }
-
 }
+
 
 function VolveAJugar() {
     pResultado1.innerHTML = "Aciertos:";
     pResultado2.innerHTML = "";
     clicks = 0;
-    aciertos=0;
+    aciertos = 0;
     generar()
 }

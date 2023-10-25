3c522b79ba800292eb0efbe7109808c105a3626a
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-10-25 12:36:55 -0300

    se comienza a guardar la información en el LocalStorage

--- memo.js
+++ memo.js
@@ -3,7 +3,11 @@ const divMatriz = document.querySelector("#matriz");
 const pResultado1 = document.querySelector("#resultado1");
 const pResultado2 = document.querySelector("#resultado2");
 
+const CLAVE_ACIERTOS = "lista de aciertos";
+const CLAVE_IMAGENES = "lista de imagenes";
 
+let listaAciertos = "lista vacía";
+let listaImagenes = "lista vacía";
 
 const imagenes = [
     "https://cdn.shopify.com/s/files/1/0530/8517/5981/files/ShopByTitle-TJ.png?v=1654799164",
@@ -18,7 +22,9 @@ const imagenes = [
 const TOTAL_ACIERTOS = 6;
 let ImgSeleccionada = 0;
 let clicks = 0;
-let aciertos=0;
+let aciertos = 0;
+listaAciertos = [];
+listaImagenes = [];
 
 function ContadorImg() {
     imagenes.forEach((imagen) => {
@@ -29,8 +35,8 @@ function ContadorImg() {
 function generar() {
     const MAX_FILA = 3;
     const MAX_COL = 4;
-    
-    
+    pResultado2.innerHTML=""
+
 
     ContadorImg();
 
@@ -57,7 +63,7 @@ function generar() {
 
         return columnas;
     }
-/* La función obtenerImagenAleatoria filtra las imágenes que no se han mostrado más de dos veces y elige aleatoriamente una de las imágenes disponibles. Luego, al hacer clic en un elemento del tablero, se muestra la imagen correspondiente y se actualiza el contador. Esto garantiza que nunca se muestren más de dos imágenes idénticas en el tablero. */
+    /* La función obtenerImagenAleatoria filtra las imágenes que no se han mostrado más de dos veces y elige aleatoriamente una de las imágenes disponibles. Luego, al hacer clic en un elemento del tablero, se muestra la imagen correspondiente y se actualiza el contador. Esto garantiza que nunca se muestren más de dos imágenes idénticas en el tablero. */
     function obtenerImagenAleatoria() {
         const imgDisp = imagenes.filter((imagen) => imagenes[imagen] < 2);
         const imgAleatoria = imgDisp[Math.floor(Math.random() * imgDisp.length)];
@@ -74,23 +80,31 @@ function seleccionar(elemento) {
 
     const imgURL = elemento.getAttribute("data-imagen");
     elemento.innerHTML = `<img class="col casilla" src="${imgURL}" alt=""></img>`;
+  
 
 
-    if (clicks == 0) {
+    if (clicks <= 2) {
         if (ImgSeleccionada === 0) {
             ImgSeleccionada = elemento;
         } else {
             if (ImgSeleccionada.getAttribute("data-imagen") === imgURL) {
-
+                elemento.style.pointerEvents = "none"
                 setTimeout(() => {
-                    aciertos ++;
+                    aciertos++;
                     pResultado1.innerHTML = "Aciertos: " + (aciertos);
+                    
+                    listaImagenes.push(imgURL)
+                    listaAciertos.push(aciertos)
+
+                    actualizarLocalStorage(); 
 
                     if (aciertos === TOTAL_ACIERTOS) {
                         pResultado1.innerHTML = "FELICITACIONES GANASTE"
                         pResultado2.innerHTML = `
-                        <button onclick="VolveAJugar()">Volver a Jugar</button>
+                        <button onclick="volveAJugar()">Volver a Jugar</button>
                         `
+                        elemento.style.pointerEvents = "none";
+                        juegoGanado = true;
                     }
                 }, 100);
                 ImgSeleccionada = 0;
@@ -103,14 +117,37 @@ function seleccionar(elemento) {
                 }, 500);
             }
         }
-    }
 
+    }
 }
 
-function VolveAJugar() {
+
+
+function actualizarLocalStorage() {
+    localStorage.setItem(CLAVE_ACIERTOS, JSON.stringify(listaAciertos));
+    localStorage.setItem(CLAVE_IMAGENES, JSON.stringify(listaImagenes));
+}
+
+function volveAJugar() {
     pResultado1.innerHTML = "Aciertos:";
     pResultado2.innerHTML = "";
     clicks = 0;
-    aciertos=0;
+    aciertos = 0;
+    juegoGanado = false;
     generar()
 }
+
+
+function partidaGuardada(aciertos, imgURL) {
+
+    
+    const listaAciertos = JSON.parse(localStorage.getItem(CLAVE_ACIERTOS));
+    const listaImagenes = JSON.parse(localStorage.getItem(CLAVE_IMAGENES));
+
+    listaImagenes.push(imgURL)
+    listaAciertos.push(aciertos)
+
+    actualizarLocalStorage()
+    generar();
+}
+
